45ce4d77d9ae6d7153b6948f5aeab010dad3147d
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-13 13:36:30 -0700

    this is working

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -1,11 +1,29 @@
-import Triangle from './../src/triangle.js';
+import Sudoku from './../src/triangle.js';
 
-describe('Triangle', () => {
+describe('Sudoku', () => {
+  let correctSudoku;
 
-  test('should correctly create a triangle object with three lengths', () => {
-    const myTriangle = new Triangle(2,4,5);
-    expect(myTriangle.side1).toEqual(2);
-    expect(myTriangle.side2).toEqual(4);
-    expect(myTriangle.side3).toEqual(5);
+  beforeEach(() => {
+    correctSudoku = new Sudoku([8, 2, 7, 1, 5, 4, 3, 9, 6],
+    [9, 6, 5, 3, 2, 7, 1, 4, 8],
+    [3, 4, 1, 6, 8, 9, 7, 5, 2],
+    [5, 9, 3, 4, 6, 8, 2, 7, 1],
+    [4, 7, 2, 5, 1, 3, 6, 8, 9],
+    [6, 1, 8, 9, 7, 2, 4, 3, 5],
+    [7, 8, 6, 2, 3, 5, 9, 1, 4],
+    [1, 5, 4, 7, 9, 6, 8, 2, 3],
+    [2, 3, 9, 8, 4, 1, 5, 6, 7]);
+  });
+
+  test ('should correctly create a Sudoku object', () => {
+    expect(correctSudoku.grid).toEqual([8, 2, 7, 1, 5, 4, 3, 9, 6],
+    [9, 6, 5, 3, 2, 7, 1, 4, 8],
+    [3, 4, 1, 6, 8, 9, 7, 5, 2],
+    [5, 9, 3, 4, 6, 8, 2, 7, 1],
+    [4, 7, 2, 5, 1, 3, 6, 8, 9],
+    [6, 1, 8, 9, 7, 2, 4, 3, 5],
+    [7, 8, 6, 2, 3, 5, 9, 1, 4],
+    [1, 5, 4, 7, 9, 6, 8, 2, 3],
+    [2, 3, 9, 8, 4, 1, 5, 6, 7]);
   });
 });
